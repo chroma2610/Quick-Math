0aba4c7fd7404e9ccfc35ec2b8122cf03ac43f23
Shiloh Harrison <shilohharrison2610@gmail.com>
2026-05-06 22:37:07 -0700

    1.0

--- quickMath.py
+++ quickMath.py
@@ -5,15 +5,22 @@ max_operand = 12
 
 operators = ["+", "-", "/", "*"]
 
+
 def generate_problem():
     left = random.randint(min_operand, max_operand)
     right = random.randint(min_operand, max_operand)
     operator = random.choice(operators)
 
     expr = str(left) + " " + operator + " " + str(right)
-    answer = eval(expr)
 
-    print(expr)
-    print(answer)
+    return expr
 
-generate_problem()
+
+while True:
+    expression = generate_problem()
+    answer = eval(expression)
+    response = input("solve: " + expression + "\n")
+    if float(response) == answer:
+        print("you done it")
+    else:
+        print("you failed, the answer was " + answer)
