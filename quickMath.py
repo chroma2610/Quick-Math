343080d9c19fa5b3c88f97b76c3b837a2de11e44
Shiloh Harrison <shilohharrison2610@gmail.com>
2026-05-06 20:48:18 -0700

    Add initial implementation of random math problem generator

---
+++ quickMath.py
@@ -0,0 +1,19 @@
+import random
+
+min_operand = 3
+max_operand = 12
+
+operators = ["+", "-", "/", "*"]
+
+def generate_problem():
+    left = random.randint(min_operand, max_operand)
+    right = random.randint(min_operand, max_operand)
+    operator = random.choice(operators)
+
+    expr = str(left) + " " + operator + " " + str(right)
+    answer = eval(expr)
+
+    print(expr)
+    print(answer)
+
+generate_problem()
